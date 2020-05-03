5fee60406336923c9e7681658cb25408c410dc54
Naomi Archibong <50037069+archibongn1@users.noreply.github.com>
2020-05-02 20:26:27 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ArithSmcho',
+    version='0.1',
+    packages=find_packages(exclude=['test*']),
+    url='',
+    license='',
+    author='smcho',
+    author_email='',
+    description=''
+)
